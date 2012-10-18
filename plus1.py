0cdcc7c913379edafc82353af1d10a44c38d0a27
Scott Graham <scottmg@chromium.org>
2012-10-18 11:38:19 -0700

    remove, replaced by csearch_filter.py

--- plus1.py
+++
@@ -1,7 +0,0 @@
-# Filter for csearch. Line numbers are 0 based, which is bad for editors.
-# Should really fix csearch, but oh well.
-import sys
-import re
-for line in sys.stdin:
-  sys.stdout.write(
-      re.sub(r':([0-9]+):', lambda x: ':%d:' % (int(x.group(1)) + 1), line))
