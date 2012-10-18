71d22de63febf9c5351e67a5498af52cc5781953
Scott Graham <scottmg@chromium.org>
2012-10-18 11:38:32 -0700

    some updates

--- munge_name_and_gvim_remote.py
+++ munge_name_and_gvim_remote.py
@@ -1,4 +1,4 @@
-# Handle file.cc(23) and open vim to right line.
+# Handle file.cc(23) or file.cc:23: and open vim to right line.
 
 import os
 import re
@@ -8,15 +8,29 @@ def run(cmd):
   #print cmd
   os.system(cmd)
 
+def open_at(file, line=1):
+  if not os.path.exists(file):
+    strip_out = ['@', '../../', '..\\..\\']
+    for strip in strip_out:
+      if file.startswith(strip):
+        file = file[len(strip):]
+        print "Using modified file path: %s" % file
+  run('start gvim.exe --remote-silent +%s "%s"' % (line, file))
+
 def main():
   for arg in sys.argv[1:]:
     mo = re.match('(.*)\((\d+)\)\s*', arg)
+    mo2 = re.match('(.*):(\d+):?\s*', arg)
     if mo:
       file = mo.group(1)
       line = mo.group(2)
-      run('start gvim.exe --remote-send %sgg --remote-silent "%s"' % (line, file))
+      open_at(file, line)
+    elif mo2:
+      file = mo2.group(1)
+      line = mo2.group(2)
+      open_at(file, line)
     else:
-      run('start gvim.exe --remote-silent "%s"' % arg)
+      open_at(arg)
 
 if __name__ == '__main__':
   main()
